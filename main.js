73f2872a3fe35df5e6ca3fe084e8ebb4554c1d02
Pankaj Pawar <Pankaj14321@protonmail.com>
2025-01-27 06:56:43 +0000

    clean data file created

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
+const fs = require("fs");
+const Papa = require("papaparse");
+
+// const { parse } = require("csv-parse/sync");
+
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
-console.log(argv.input, argv.output);
-console.log(process.env.TOKEN);
+
+const inputFile = argv.input;
+const outputFile = argv.clean;
+const reportFile = argv.report;
+
+
+
+
+
+function readCsv(file) {
+  const fileContent = fs.readFileSync(inputFile, "utf8");
+  const records = Papa.parse(fileContent,{
+    header: true,
+  });
+  const headers= records.meta.fields;
+  const body = records.data;
+  return {headers,body};
+}
+
+function writeCsv(path,data) {
+    const stringify=Papa.unparse(data);
+  fs.writeFileSync(path, stringify);
+}
+writeCsv(outputFile,readCsv(inputFile).body);
